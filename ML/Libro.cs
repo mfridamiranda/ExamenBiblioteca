bcb46811058e34ede01615505bb203da20f8f7c5
Kevin Garcia <K.Garcia.Vega.61@gmail.com>
2022-12-09 15:07:22 -0600

    Add project files.

---
+++ ML/Libro.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ML
+{
+    public class Libro
+    {
+       public int IdLibro { get; set; }
+
+        public string NombreLibro { get; set; }
+
+        public int IdAutor { get; set; }
+        public string NumeroPaginas { get; set; }
+        public string FechaPublicacion { get; set; }
+        public int IdEditorial { get; set; }
+        public string Edicion { get; set; }
+        public int IdGenero { get; set; }
+
+    }
+}
